b92e0d8a52c0a1f6b55a338962e178c373adcfd4
henry_lab <wgj601@gmail.com>
2026-05-14 10:40:40 +0800

    更新csv

--- src/LocalMapping.cc
+++ src/LocalMapping.cc
@@ -1057,7 +1057,7 @@ void LocalMapping::SearchInNeighbors()
     // Extend to some second neighbors if abort is not requested
     for(int i=0, imax=vpTargetKFs.size(); i<imax; i++)
     {
-        const vector<KeyFrame*> vpSecondNeighKFs = vpTargetKFs[i]->GetBestCovisibilityKeyFrames(5);//這裡是將第一層鄰居的前五個鄰居加入到第二層鄰居中,開關這裡可以讓 localmapping 的迴圈更快結束，方便 debug localmapping 的部分
+        const vector<KeyFrame*> vpSecondNeighKFs = vpTargetKFs[i]->GetBestCovisibilityKeyFrames(10);//20這裡是將第一層鄰居的前五個鄰居加入到第二層鄰居中,開關這裡可以讓 localmapping 的迴圈更快結束，方便 debug localmapping 的部分
         for(vector<KeyFrame*>::const_iterator vit2=vpSecondNeighKFs.begin(), vend2=vpSecondNeighKFs.end(); vit2!=vend2; vit2++)
         {
             KeyFrame* pKFi2 = *vit2;
